921aa743eabf14d02e8c2f7e90790d30c2e99a17
unknown <carlos.barreraacuna@gmail.com>
2024-05-09 01:29:35 -0500

    Creando pagina not-found para las categorias personalizadas

---
+++ src/app/(shop)/category/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+import { PageNotFound } from "@/app/components/ui/not-found/PageNotFound";
+
+import Link from "next/link"
+
+export default function NotFound() {
+    return (
+      
+      <PageNotFound/>
+    );
+  }
--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,22 @@
-export default function Category() {
-    return (
+import {notFound} from "next/navigation";
+
+interface Props {
+  params : {
+    id: string;
+  }
+}
+
+export default function Category({params}: Props) {
+
+  const {id}= params;
+
+  if (id === 'kids') {
+    notFound();
+  }
+
+    return ( 
       <div>
-        <h1>Category page</h1>
+        <h1>Category page {id}</h1>
         
         
       </div>
