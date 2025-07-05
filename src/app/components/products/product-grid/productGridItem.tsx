0050e3fe64760fa7dea2d991a67e31bc842def41
unknown <carlos.barreraacuna@gmail.com>
2025-07-05 10:11:33 -0500

    cambiar imagen - MouseOver de los productos

--- src/app/components/products/product-grid/productGridItem.tsx
+++ src/app/components/products/product-grid/productGridItem.tsx
@@ -1,32 +1,45 @@
-import { Product } from "@/interfaces";
-import Image from "next/image";
-import Link from "next/link"
+'use client';
+
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Product } from '@/interfaces';
+import { useState } from 'react';
 
 interface Props {
-    product : Product;
+  product: Product;
 }
 
-export const ProductGridItem = ({ product }: Props) => {
-    return (
-        <div className="rounded-md overflow-hidden fade-in">
-            <Link href={ `/Product/${ product.slug }`}>
-            
-            <Image
-            src= {`/products/${ product.images[0] }`}
-            alt={ product.title}
-            className="w-full object-cover"
-            width={ 500 }
-            height={ 500 }
 
-            />
-            </Link>
-            <div className="p-4 flex flex-col">
-                
-                <Link className="hover:text-blue-600 " href={ `/Product/${ product.slug }`}>
-                    { product.title}
-                </Link>
-                <span className="font-bold">${ product.price}</span>
-            </div>
-        </div>
-    )
-}
+export const ProductGridItem = ( { product }: Props ) => {
+
+  const [ displayImage, setDisplayImage ] = useState( product.images[ 0 ] );
+
+
+  return (
+    <div className="rounded-md overflow-hidden fade-in">
+      <Link href={ `/product/${ product.slug }` }>
+        <Image
+          src={ `/products/${ displayImage }` }
+          alt={ product.title }
+          className="w-full object-cover rounded"
+          width={ 500 }
+          height={ 500 }
+          onMouseEnter={ () => setDisplayImage( product.images[1] )  }
+          onMouseLeave={ () => setDisplayImage( product.images[0] ) }
+        />
+      </Link>
+
+      <div className="p-4 flex flex-col">
+        <Link
+          className="hover:text-blue-600"
+          href={ `/product/${ product.slug }` }>
+          { product.title }
+        </Link>
+        <span className="font-bold">${ product.price }</span>
+      </div>
+
+    </div>
+  );
+};
